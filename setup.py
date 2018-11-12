1be21778e877c383f262a7f4292804be7266e572
Briar Harrison <harri25j@mtholyoke.edu>
2018-11-12 16:29:09 -0500

    renamed - underscore

--- setup.py
+++ setup.py
@@ -3,8 +3,5 @@ from distutils.core import setup
 setup(
     name='USPSService',
     version='0.1',
-    author='Briar Harrison',
-    author_email='briar.harrison@voterlabs.com',
-    url='voterlabs.com',
     packages=['uspsservice']
 )
