768c2c260d31f96e5bc949593881d6871bb0940a
Briar Harrison <harri25j@mtholyoke.edu>
2018-11-29 14:06:13 -0500

    Added clarrifying comment to jenkins/prebuild Changed version number to three digits as per convention Change name of requirements to test_requirements to show usage more clearly. Run requirements are included in the packaging information (see setup.py)

--- jenkins/prebuild.sh
+++ jenkins/prebuild.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
+# pip has a max path length of 126 characters which causes an issue with
+# Jenkins whose paths are very long. To solve it, we store the virtual env
+# in a temp directory instead.
 rm -rf ~/temp
 mkdir -p ~/temp
 TEMP_DIR=$(mktemp -d -p ~/temp/)
 echo $TEMP_DIR > ~/temp/.venv
 virtualenv -p python3 $TEMP_DIR
 source $TEMP_DIR/bin/activate
-pip install -r requirements.txt
+pip install -r test_requirements.txt
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='USPSService',
-    version='0.1',
+    version='0.1.0',
     packages=['usps_service'],
     install_requires=[
     	'xmltodict',
